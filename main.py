f69f495ae7dee751757d213a0e8cd7e31cbee42e
Tirush Venamadala <84138572+TirushV@users.noreply.github.com>
2022-10-21 17:38:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ s3 = boto3.client('s3')
 
 print('Original Object: ')
 original = s3.get_object(
-  Bucket='elasticsearchtos3bucketlogs',
+  Bucket='<s3-bucket-name>',
   Key='title.txt')
 print(original['Body'].read().decode('utf-8'))
 
 print('Processed Object: ')
 transformed = s3.get_object(
-  Bucket='arn:aws:s3-object-lambda:ap-south-1:825957044315:accesspoint/title-transform',
+  Bucket='arn:aws:s3-object-lambda:ap-south-1:<AWS-Account>:accesspoint/title-transform',
   Key='title.txt')
 print(transformed['Body'].read().decode('utf-8'))
